a9ae115400ab837615606a2e52cd004072164df0
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <18655689+pompossum@users.noreply.github.com>
2019-11-14 14:21:55 -0500

    CUTSCENE BABYYYYYYY
    only fun part of the project is done

---
+++ SpaceGame/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public string LevelToLoad;
+    //float currCountdownValue;
+
+    void Start()
+    {
+        GameObject.Find("Text");
+    }
+    void Update()
+    {
+        transform.Translate(new Vector3(0.0f, 45f, 0.0f) * Time.deltaTime * 1f);
+
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+    //public IEnumerator StartCountdown(float countdownValue = 10)
+    //{
+    //    currCountdownValue = countdownValue;
+    //    while (currCountdownValue > 0)
+    //    {
+    //        Debug.Log("Countdown: " + currCountdownValue);
+    //        yield return new WaitForSeconds(1.0f);
+    //        currCountdownValue--;
+    //    }
+
+    //    if (currCountdownValue == 0)
+    //    {
+    //        SceneManager.LoadScene(LevelToLoad);
+    //    }
+    //}
+}
